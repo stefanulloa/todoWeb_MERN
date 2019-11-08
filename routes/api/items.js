8c1aa4e376a707d0deef2b5aa8bbe114c314430d
Gary Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2019-11-08 19:50:38 +0100

    backend complete: create and delete added

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,7 +13,33 @@ router.get('/', (req, res) => {
     //find() is a mongodb method, it returns a promise
     Item.find()
         .sort({date: -1})
-        .then(items => res.json(items))
+        .then(items => res.json(items));
 });
+
+// @route POST api/items
+// @description Create Item
+// @access Public
+router.post('/', (req, res) => {
+    //we only pass name because date is taken care of by mongodb
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    //actually saving it in db, then show item as json obj
+    newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items/:id
+// @description Delete Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    //mongodb method
+    //we are returning a message as response
+    //in case the id item does not exist, we catch an show a 404 response
+    Item.findById(req.params.id)
+        .then(item => item.remove().then( () => res.json({success: true})))
+        .catch(err => res.status(404).json({msg: "item with that id not found"}));
+});
+    
 
 module.exports = router; 
