68046e8ba55dd26da87278278e393db598be249d
Gary Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2019-11-14 23:11:05 +0100

    removing use of redux chrome dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,15 @@
-import { createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-    rootReducer, 
-    initialState, 
-    //compose is needed to pass multiple enhancers
-    compose(
-        applyMiddleware(...middleware),
-        //needed to use redux chrome dev tools
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
 );
 
 export default store;
