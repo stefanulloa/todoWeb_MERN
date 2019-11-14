e92f30df16c69956992631d5e39c5ede91f4fb72
Gary Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2019-11-14 22:16:10 +0100

    preparing project for deployment

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
 app.use(express.json());
 
 //access mongoDB URI
-const db = require('./config/keys.js').mongoURI;
-
-//any command relating to /api/items excute "items" functions
-app.use('/api/items', items);
+const db = require("./config/keys.js").mongoURI;
 
 //connect to mongo
 //.then because connect() allows promise, we can catch errors with .catch
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+//any command relating to /api/items excute "items" functions
+app.use("/api/items", items);
+
+//provide static assets in production
+if (process.env.NODE_ENV === "production") {
+  //setting static folder
+  app.use(express.static("client/build"));
+
+  //when in production and as long as the path is not "/api/items", this will be detected
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //it could be on an external server, or port 5000
 const port = process.env.PORT || 5000;
