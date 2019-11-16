543fc0ea93fcdb3b28c36322d0f3a4aec9977d6c
Gary Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2019-11-16 21:17:41 +0100

    simple user registering func and untracking of config files (sensitive data)

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://firstU:firstU8@cluster0-dl3dl.mongodb.net/test?retryWrites=true&w=majority'
-};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 //item model
 const User = require('../../models/User');
@@ -7,9 +8,45 @@ const User = require('../../models/User');
 // @route POST api/users
 // @description Register user
 // @access Public
-// only / because we already in api/items, not if we were in server.js
 router.post('/', (req, res) => {
-    res.send('register');
+    //using destructuring to access the desired passed data
+    const { name, email, password } = req.body;
+
+    //simple validation
+    if (!name || !email || !password) {
+        return res.status(400).json({ msg: 'Enter all fields' });
+    }
+
+    //checking for existing user
+    //same as "{ email: email }"
+    User.findOne({ email }).then(user => {
+        if (user) return res.status(400).json({ msg: 'User already exists' });
+
+        //if it doesnt exist
+        const newUser = new User({
+            name,
+            email,
+            password
+        });
+
+        //create salt (the bigger the number is, the more secure but also the more time it takes; 10 is common value) and hash
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                //we are actually storing the hashed password on the db
+                newUser.password = hash;
+                newUser.save().then(user => {
+                    res.json({
+                        user: {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email
+                        }
+                    });
+                });
+            });
+        });
+    });
 });
 
 module.exports = router;
