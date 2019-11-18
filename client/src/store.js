3b1613553a8ca41f224e12a61fccd125b1bc3cf5
Gary Stefano Ulloa <44326066+stefanulloa@users.noreply.github.com>
2019-11-18 19:20:34 +0100

    recovering redux devtools behaviour

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,22 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
 
 const store = createStore(
-  rootReducer,
-  initialState,
-  applyMiddleware(...middleware)
+    rootReducer,
+    initialState,
+    //compose is needed to pass multiple enhancers
+    compose(
+        applyMiddleware(...middleware),
+        //needed to use redux chrome dev tools
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    //when in production, change to: applyMiddleware(...middleware)
 );
 
 export default store;
